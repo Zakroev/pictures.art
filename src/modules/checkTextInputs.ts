e4c0d647b658a00d5d8ac61ffcee753a5aae9091
Zakroev Andrey <Dr.zakroev@yandex.ru>
2023-10-27 18:27:08 +0300

    add regex pattern

--- src/modules/checkTextInputs.ts
+++ src/modules/checkTextInputs.ts
@@ -1,3 +1,5 @@
+const nameRegexPattern = /[^а-яё 0-9]/gi;
+
 const checkTextInputs = (selector: string) => {
   const txtInputs = Array.from(
     document.querySelectorAll<HTMLInputElement>(selector)
@@ -6,7 +8,7 @@ const checkTextInputs = (selector: string) => {
   txtInputs.forEach((input) => {
     input.addEventListener('input', () => {
       const inputValue = input.value;
-      const validValue = inputValue.replace(/[^а-яё 0-9]/gi, '');
+      const validValue = inputValue.replace(nameRegexPattern, '');
       if (inputValue !== validValue) {
         input.value = validValue;
       }
