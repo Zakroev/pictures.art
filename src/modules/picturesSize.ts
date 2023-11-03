431c0f61b3636a93ade6951b7b18a4ab92e9a482
Zakroev Andrey <Dr.zakroev@yandex.ru>
2023-11-03 21:15:54 +0300

    fix problem with img

--- src/modules/picturesSize.ts
+++ src/modules/picturesSize.ts
@@ -1,30 +1,32 @@
 const picturesSize = (imgSelector: string) => {
   const blocks = document.querySelectorAll(imgSelector);
 
-  const showImg = (block: HTMLElement) => {
+  const showImg = (block: HTMLElement, imgPath: string) => {
     const img = block.querySelector('img') as HTMLImageElement;
-    img.src = img.src.slice(0, -4) + '-1.png';
+    img.src = imgPath;
     Array.from(block.querySelectorAll('p:not(.sizes-hit)')).forEach((p) => {
       const element = p as HTMLElement;
       element.style.display = 'none';
     });
   };
 
-  const hideImg = (block: HTMLElement) => {
+  const hideImg = (block: HTMLElement, imgPath: string) => {
     const img = block.querySelector('img') as HTMLImageElement;
-    img.src = img.src.slice(0, -6) + '.png';
+    img.src = imgPath;
     Array.from(block.querySelectorAll('p:not(.sizes-hit)')).forEach((p) => {
       const element = p as HTMLElement;
       element.style.display = 'block';
     });
   };
 
-  blocks.forEach((block) => {
+  blocks.forEach((block, index) => {
+    const imgPath = `/imagesSize/sizes-${index + 1}-1.png`;
+
     block.addEventListener('mouseover', () => {
-      showImg(block as HTMLElement);
+      showImg(block as HTMLElement, imgPath);
     });
     block.addEventListener('mouseout', () => {
-      hideImg(block as HTMLElement);
+      hideImg(block as HTMLElement, imgPath);
     });
   });
 };
