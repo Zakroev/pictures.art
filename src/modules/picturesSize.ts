3d56fb8abded62867af8b6c15e3038e010e805b6
Zakroev Andrey <Dr.zakroev@yandex.ru>
2023-11-06 13:03:27 +0300

    try to add small pictures

--- src/modules/picturesSize.ts
+++ src/modules/picturesSize.ts
@@ -1,15 +1,26 @@
 const picturesSize = (imgSelector: string) => {
   const blocks = document.querySelectorAll(imgSelector);
 
-  const showImg = (block: HTMLElement, imgPath: string) => {
+  const showImg = (
+    block: HTMLElement,
+    smallImgPath: string,
+    bigImgPath: string
+  ) => {
     const img: HTMLImageElement | null = block.querySelector('img');
     if (img) {
       img.dataset.originalSrc = img.src;
-      img.src = imgPath;
+      img.src = smallImgPath;
+
       Array.from(block.querySelectorAll('p:not(.sizes-hit)')).forEach((p) => {
         const element = p as HTMLElement;
         element.style.display = 'none';
       });
+
+      const bigImg = new Image();
+      bigImg.src = bigImgPath;
+      bigImg.onload = () => {
+        img.dataset.originalSrc = bigImgPath;
+      };
     }
   };
 
@@ -17,6 +28,7 @@ const picturesSize = (imgSelector: string) => {
     const img: HTMLImageElement | null = block.querySelector('img');
     if (img) {
       img.src = img.dataset.originalSrc || '';
+
       Array.from(block.querySelectorAll('p:not(.sizes-hit)')).forEach((p) => {
         const element = p as HTMLElement;
         element.style.display = 'block';
@@ -25,11 +37,13 @@ const picturesSize = (imgSelector: string) => {
   };
 
   blocks.forEach((block, index) => {
-    const imgPath = `/imagesSize/sizes-${index + 1}-1.png`;
+    const smallImgPath = `/imagesSize/sizes-${index + 1}-small.png`;
+    const bigImgPath = `/imagesSize/sizes-${index + 1}-1.png`;
 
     block.addEventListener('mouseover', () => {
-      showImg(block as HTMLElement, imgPath);
+      showImg(block as HTMLElement, smallImgPath, bigImgPath);
     });
+
     block.addEventListener('mouseout', () => {
       hideImg(block as HTMLElement);
     });
